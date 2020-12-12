4a08c8ca4822bd00431a3c7db521b58d9eb9196a
Pedro Santos <donbonifacio@gmail.com>
2020-12-12 14:32:38 +0000

    Add project template

---
+++ template/src/Solution.clj
@@ -0,0 +1,8 @@
+(ns Solution
+  (:gen-class))
+
+(defn -main
+  "The main function, will read data from the stdin and call the worker
+  function."
+  [& args]
+  (println "Hello"))
---
+++ template/test/Solution_test.clj
@@ -0,0 +1,7 @@
+(ns Solution-test
+  (:require [clojure.test :refer :all]
+            [Solution :as solution]))
+
+(deftest example-test
+  (is (= true true)))
+
