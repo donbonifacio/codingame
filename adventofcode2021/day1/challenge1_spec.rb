d0acd81d6b6c2d3c5bacab177a8e6dd743330491
Pedro Santos <donbonifacio@gmail.com>
2021-12-02 19:55:19 +0000

    Add solution for input

--- adventofcode2021/day1/challenge1_spec.rb
+++ adventofcode2021/day1/challenge1_spec.rb
@@ -1,9 +1,17 @@
 require_relative './challenge1.rb'
 
-RSpec.describe 'Day1.1' do
-  it 'runs the default example' do
-    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    expect(Day1.measure_increases(data)).to eq(7)
+RSpec.describe Day1 do
+  context 'when running #measure_increases' do
+    it 'runs the default example' do
+      data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+      expect(Day1.measure_increases(data)).to eq(7)
+    end
 
+    it 'runs the input file' do
+      data = File.read('./input.txt').split(/\s/).map(&:to_i)
+
+      expect(data.count).to eq(2000)
+      expect(Day1.measure_increases(data)).to eq(1195)
+    end
   end
 end
