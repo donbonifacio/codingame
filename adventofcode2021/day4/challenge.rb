05fbed71ef2e0271478461036be22c085a15148e
Pedro Santos <donbonifacio@gmail.com>
2021-12-05 11:09:48 +0000

    Day 4 - Passing test data

--- adventofcode2021/day4/challenge.rb
+++ adventofcode2021/day4/challenge.rb
@@ -24,14 +24,58 @@ module Day4
       matrix[x_idx][y_idx]
     end
 
-    def score
-      1
+    def play_number(number)
+      5.times do |x|
+        5.times do |y|
+          matrix[x][y] = '*' if at(x, y) == number
+        end
+      end
+    end
+
+    def won?
+      matrix.each do |line|
+        return true if line.join == '*****'
+      end
+
+      5.times do |idx|
+        return true if matrix.map { |line| line[idx] }.join == '*****'
+      end
+
+      false
+    end
+
+    def score(drawn_number)
+      unmarked = matrix
+                 .flatten
+                 .reject { |entry| entry == '*' }
+                 .sum
+
+      unmarked * drawn_number
     end
   end
 
   def winner_score(raw_bingo)
     bingo = deserialize(raw_bingo)
-    board_score(Board.new)
+    result = play_until_winner(bingo)
+    raise 'No winner' unless result[:winner]
+
+    result[:board].score(result[:winner_number])
+  end
+
+  def play_until_winner(bingo)
+    bingo.draw_numbers.each do |drawn_number|
+      # puts "-- Number: #{drawn_number}"
+      bingo.boards.each do |board|
+        board.play_number(drawn_number)
+        if board.won?
+          # puts "Won? #{board.won?} #{board.inspect}"
+          return { winner: true,
+                   board: board,
+                   winner_number: drawn_number }
+        end
+      end
+    end
+    { winner: false }
   end
 
   def board_score(board)
--- adventofcode2021/day4/challenge_spec.rb
+++ adventofcode2021/day4/challenge_spec.rb
@@ -1,6 +1,8 @@
 require_relative './challenge.rb'
 
-TEST_DATA = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+TEST_DRAW_NUMBERS = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1".freeze
+
+TEST_BOARDS = "
 
 22 13 17 11  0
  8  2 23  4 24
@@ -19,6 +21,8 @@ TEST_DATA = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,2
 18  8 23 26 20
 22 11 13  6  5
  2  0 12  3  7".freeze
+
+TEST_DATA = (TEST_DRAW_NUMBERS + TEST_BOARDS).freeze
 # INPUT_DATA = File.read('./input.txt').split(/\s/).map(&:to_i).freeze
 
 RSpec.describe Day4 do
@@ -32,8 +36,13 @@ RSpec.describe Day4 do
       expect(bingo.boards[2].at(3, 3)).to eql(6)
     end
 
-    xit 'has expected data for TEST_DATA' do
+    it 'has expected data for TEST_DATA' do
       expect(Day4.winner_score(TEST_DATA)).to eq(4512)
+    end
+
+    it 'has expected data for TEST_DATA but for a column win' do
+      data = ('24,9,26,6,3'.freeze + TEST_BOARDS).freeze
+      expect(Day4.winner_score(data)).to eq(771)
     end
   end
 end
