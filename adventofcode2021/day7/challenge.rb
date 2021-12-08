50a4407711ed0f85848f292705389efc3d1a952e
Pedro Santos <donbonifacio@gmail.com>
2021-12-08 08:55:03 +0000

    Day 7 - Passing examples but not input

--- adventofcode2021/day7/challenge.rb
+++ adventofcode2021/day7/challenge.rb
@@ -7,12 +7,26 @@ module Day7
     cheapest = median(positions)
     positions
       .map { |pos| (pos - cheapest).abs }
-      .reduce(&:+)
+      .sum
   end
 
   def median(array)
     sorted = array.sort
     len = sorted.length
     ((sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0).round
+  end
+
+  def expensive_fuel_cost(data)
+    positions = data.split(',').map(&:strip).map(&:to_i)
+    cheapest = average(positions)
+    positions
+      .map { |pos| (pos - cheapest).abs.downto(1).reduce(&:+) }
+      .compact
+      .sum
+  end
+
+  def average(array)
+    sum = array.sum
+    (sum.to_f / array.length).round
   end
 end
--- adventofcode2021/day7/challenge_spec.rb
+++ adventofcode2021/day7/challenge_spec.rb
@@ -8,11 +8,19 @@ RSpec.describe Day7 do
     it 'has expected result for TEST_DATA' do
       expect(Day7.fuel_cost(TEST_DATA)).to eq(37)
     end
+
+    it 'has expected result for TEST_DATA for expensive move' do
+      expect(Day7.expensive_fuel_cost(TEST_DATA)).to eq(168)
+    end
   end
 
   context 'when working with input data' do
     it 'has expected result for INPUT_DATA' do
       expect(Day7.fuel_cost(INPUT_DATA)).to eq(355_521)
+    end
+
+    it 'has expected result for TEST_DATA for expensive move' do
+      expect(Day7.expensive_fuel_cost(INPUT_DATA)).to eq(168)
     end
   end
 end
