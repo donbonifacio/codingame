c8ff504d606ac0cf167024aa428bdbf683e7286b
Pedro Santos <donbonifacio@gmail.com>
2022-12-02 14:44:00 +0000

    Add part2

--- adventofcode/2022/day02/challenge.go
+++ adventofcode/2022/day02/challenge.go
@@ -12,44 +12,61 @@ func main() {
 	fmt.Printf("Challenge %v\n", challengeId)
 }
 
-func calculateScore(data string) int {
+func sumScore(data string, calculator func(string, string) int) int {
 	lines := strings.Split(strings.TrimSpace(data), "\n")
 	sum := 0
 	for _, line := range lines {
 		plays := strings.Split(strings.TrimSpace(line), " ")
 		player1 := plays[0]
 		player2 := plays[1]
-		sum += calculatePlayScore(player1, player2)
+		sum += calculator(player1, player2)
 	}
 	return sum
 }
 
 var playScores = map[string]int{
-	"A": 1, // R
-	"B": 2, // P
-	"C": 3, // S
-
 	"X": 1, // R
 	"Y": 2, // P
 	"Z": 3, // S
 }
+var resultMapPart1 = map[string]int{
+	"AX": 3,
+	"BX": 0,
+	"CX": 6,
 
-func calculatePlayScore(player1 string, player2 string) int {
-	playScore1 := playScores[player1]
+	"AY": 6,
+	"BY": 3,
+	"CY": 0,
+
+	"AZ": 0,
+	"BZ": 6,
+	"CZ": 3,
+}
+
+func part1calculator(player1 string, player2 string) int {
 	playScore2 := playScores[player2]
-
-	resultScore := 0
-	if playScore1 == playScore2 {
-		resultScore = 3
-	} else if player1 == "A" && player2 == "Y" {
-		resultScore = 6
-	} else if player1 == "B" && player2 == "Z" {
-		resultScore = 6
-	} else if player1 == "C" && player2 == "X" {
-		resultScore = 6
-	}
+	resultScore := resultMapPart1[fmt.Sprintf("%v%v", player1, player2)]
 
 	return resultScore + playScore2
+}
+
+var resultMapPart2 = map[string]string{
+	"AX": "Z",
+	"BX": "X",
+	"CX": "Y",
+
+	"AY": "X",
+	"BY": "Y",
+	"CY": "Z",
+
+	"AZ": "Y",
+	"BZ": "Z",
+	"CZ": "X",
+}
+
+func part2calculator(player1 string, player2 string) int {
+	newPlayer2 := resultMapPart2[fmt.Sprintf("%v%v", player1, player2)]
+	return part1calculator(player1, newPlayer2)
 }
 
 func readInput(filename string) string {
--- adventofcode/2022/day02/challenge_test.go
+++ adventofcode/2022/day02/challenge_test.go
@@ -13,28 +13,46 @@ func TestMain(t *testing.T) {
 
 func TestSample(t *testing.T) {
 	input := readInput("sample.txt")
-	score := calculateScore(input)
+	score := sumScore(input, part1calculator)
 	assert.Equal(t, 15, score)
 }
 
 func TestInput(t *testing.T) {
 	input := readInput("input.txt")
-	score := calculateScore(input)
+	score := sumScore(input, part1calculator)
 	assert.Equal(t, 9177, score)
 }
 
 func TestCalculatePlayerScore(t *testing.T) {
-	assert.Equal(t, 6+1, calculatePlayScore("C", "X"))
-	assert.Equal(t, 6+2, calculatePlayScore("A", "Y"))
-	assert.Equal(t, 6+3, calculatePlayScore("B", "Z"))
+	assert.Equal(t, 6+1, part1calculator("C", "X"))
+	assert.Equal(t, 6+2, part1calculator("A", "Y"))
+	assert.Equal(t, 6+3, part1calculator("B", "Z"))
 
-	assert.Equal(t, 1, calculatePlayScore("B", "X"))
-	assert.Equal(t, 6, calculatePlayScore("C", "Z"))
+	assert.Equal(t, 1, part1calculator("B", "X"))
+	assert.Equal(t, 6, part1calculator("C", "Z"))
 
-	assert.Equal(t, 2, calculatePlayScore("C", "Y"))
-	assert.Equal(t, 3, calculatePlayScore("A", "Z"))
+	assert.Equal(t, 2, part1calculator("C", "Y"))
+	assert.Equal(t, 3, part1calculator("A", "Z"))
 
-	assert.Equal(t, 3+1, calculatePlayScore("A", "X"))
-	assert.Equal(t, 3+2, calculatePlayScore("B", "Y"))
-	assert.Equal(t, 3+3, calculatePlayScore("C", "Z"))
+	assert.Equal(t, 3+1, part1calculator("A", "X"))
+	assert.Equal(t, 3+2, part1calculator("B", "Y"))
+	assert.Equal(t, 3+3, part1calculator("C", "Z"))
+}
+
+func TestSamplePart2(t *testing.T) {
+	input := readInput("sample.txt")
+	score := sumScore(input, part2calculator)
+	assert.Equal(t, 12, score)
+}
+
+func TestInputPart2(t *testing.T) {
+	input := readInput("input.txt")
+	score := sumScore(input, part2calculator)
+	assert.Equal(t, 12111, score)
+}
+
+func TestCalculatePlayerScorePart2(t *testing.T) {
+	assert.Equal(t, 4, part2calculator("A", "Y"))
+	assert.Equal(t, 1, part2calculator("B", "X"))
+	assert.Equal(t, 7, part2calculator("C", "Z"))
 }
