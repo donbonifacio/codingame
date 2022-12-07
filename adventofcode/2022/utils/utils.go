d5501e4665f799548f5515c1532ee40e8a70fc00
Pedro Santos <donbonifacio@gmail.com>
2022-12-07 21:23:17 +0000

    Add more utilities

--- adventofcode/2022/utils/utils.go
+++ adventofcode/2022/utils/utils.go
@@ -17,6 +17,43 @@ func Atoi(raw string) int {
 	return val
 }
 
+func AsLines(data string) []string {
+	return Split(data, "\n")
+}
+
+func Split(data string, separator string) []string {
+	parts := strings.Split(strings.TrimSpace(data), separator)
+	for i := 0; i < len(parts); i++ {
+		parts[i] = strings.TrimSpace(parts[i])
+	}
+	return parts
+}
+
+func IsEmpty(data string) bool {
+	return strings.TrimSpace(data) == ""
+}
+
+func EachString(data []string, op func(str string)) {
+	for i := 0; i < len(data); i++ {
+		item := data[i]
+		op(item)
+	}
+}
+
+func MinMax(array []int) (int, int) {
+	var max int = array[0]
+	var min int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
 func ReadInput(filename string) string {
 	data, err := os.ReadFile(filename)
 	if err != nil {
--- adventofcode/2022/utils/utils_test.go
+++ adventofcode/2022/utils/utils_test.go
@@ -19,3 +19,27 @@ func TestReadNumberFile(t *testing.T) {
 	nums := ReadFileWithNumbers("tmp.txt")
 	assert.Equal(t, nums, []int{1, 2, 4})
 }
+
+func TestAsLines(t *testing.T) {
+	assert.Equal(t, []string{"1", "2", "3"}, AsLines("1\n 2\n3\n\n"))
+}
+
+func TestEachString(t *testing.T) {
+	data := []string{"a", "b", "c"}
+	output := ""
+	EachString(data, func(str string) {
+		output = output + str
+	})
+	assert.Equal(t, "abc", output)
+}
+
+func TestIsEmpty(t *testing.T) {
+	assert.Equal(t, true, IsEmpty(""))
+	assert.Equal(t, false, IsEmpty("ddd"))
+}
+
+func TestMinMax(t *testing.T) {
+	min, max := MinMax([]int{1, 3, 3, 0, 10, 5})
+	assert.Equal(t, 0, min)
+	assert.Equal(t, 10, max)
+}
