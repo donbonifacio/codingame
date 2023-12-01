e6d78dcd8fd69089e1070b1221ef6be92f531f1c
Pedro Santos <donbonifacio@gmail.com>
2023-12-01 17:56:25 +0000

    Day1.2

--- adventofcode/2023/day01/challenge.go
+++ adventofcode/2023/day01/challenge.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"unicode"
 
 	"github.com/donbonifacio/codingame/blob/master/adventofcode/2023/utils"
@@ -34,7 +35,38 @@ func part1(data []string) int {
 		}
 		return utils.Atoi(first + last)
 	})
+	//fmt.Println(values)
 	return lo.Reduce(values, func(agg int, item int, _ int) int {
 		return agg + item
 	}, 0)
+}
+
+func part2(data []string) int {
+	dict := [][]string{
+		[]string{"seven", "7"},
+		[]string{"nine", "9"},
+		[]string{"one", "1"},
+		[]string{"eight", "8"},
+		[]string{"two", "2"},
+		[]string{"three", "3"},
+		[]string{"four", "4"},
+		[]string{"five", "5"},
+		[]string{"six", "6"},
+	}
+	transformed := lo.Map(data, func(line string, index int) string {
+		newLine := line
+		for i := 0; i < len(newLine); i++ {
+			rest := newLine[i:]
+			for _, v := range dict {
+				if strings.HasPrefix(rest, v[0]) {
+					newLine = strings.Replace(newLine, v[0], v[1], 1)
+					//fmt.Printf("restt: %v, prefix: %v %v -> %v \n", rest, v[0], v[1], newLine)
+				}
+			}
+		}
+
+		return newLine
+	})
+	//fmt.Println(transformed)
+	return part1(transformed)
 }
--- adventofcode/2023/day01/challenge_test.go
+++ adventofcode/2023/day01/challenge_test.go
@@ -13,3 +13,18 @@ func TestMain(t *testing.T) {
 func TestMain_sample(t *testing.T) {
 	assert.Equal(t, part1(readInput("input.txt")), 53334, "they should be equal")
 }
+
+func TestMain_sample2(t *testing.T) {
+	assert.Equal(t, part2(readInput("sample2.txt")), 281, "they should be equal")
+}
+
+func TestMain_input2(t *testing.T) {
+	assert.Equal(t, part2(readInput("input2.txt")), 52834, "they should be equal")
+}
+
+func Test_values(t *testing.T) {
+	assert.Equal(t, part2([]string{"oneight1"}), 11, "they should be equal")
+	assert.Equal(t, part2([]string{"eightwo1"}), 81, "they should be equal")
+	assert.Equal(t, part2([]string{"twone1"}), 21, "they should be equal")
+	assert.Equal(t, part2([]string{"sevenine1"}), 71, "they should be equal")
+}
