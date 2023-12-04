b775d22065880a5aec7aebe2d7984fad64f5d5a5
Pedro Santos <donbonifacio@gmail.com>
2023-12-04 20:13:14 +0000

    Day 4.2

--- adventofcode/2023/day04/challenge.go
+++ adventofcode/2023/day04/challenge.go
@@ -19,6 +19,7 @@ type Card struct {
 	numbers []int
 	matches []int
 	worth   int
+	copies  []int
 }
 
 func readInput(filename string) []string {
@@ -72,15 +73,54 @@ func part1(data []string) int {
 	cards = lo.Map(cards, loadMatches)
 	cards = lo.Map(cards, calculateWorth)
 
-	lo.ForEach(cards, func(card Card, _ int) {
-		fmt.Printf("Card %v: matches:%v worth:%v\n", card.id, card.matches, card.worth)
-	})
+	/*
+		lo.ForEach(cards, func(card Card, _ int) {
+			fmt.Printf("Card %v: matches:%v worth:%v\n", card.id, card.matches, card.worth)
+		})
+	*/
 
 	return lo.Reduce(cards, func(agg int, card Card, _ int) int {
 		return agg + card.worth
 	}, 0)
 }
 
+func calculateCopies(card Card, _ int) Card {
+	card.copies = lo.Map(card.matches, func(match int, index int) int {
+		return card.id + index + 1
+	})
+	return card
+}
+
+func gatherInstances(instances map[int]int, allCards []Card, cards []Card, run int) {
+	if run > 2 {
+		//return
+	}
+	for _, card := range cards {
+		id := card.id
+		instances[id] += 1
+		//fmt.Printf("%v +1\n", id)
+		lo.ForEach(card.copies, func(childId int, _ int) {
+			index := childId - 1
+			//fmt.Printf(" --- %v spawn %v:%v\n", id, childId, allCards[index])
+			gatherInstances(instances, allCards, []Card{allCards[index]}, run+1)
+		})
+
+	}
+}
+
 func part2(data []string) int {
-	return 0
+	cards := lo.Map(data, parseCard)
+	cards = lo.Map(cards, loadMatches)
+	cards = lo.Map(cards, calculateCopies)
+
+	instances := map[int]int{}
+	gatherInstances(instances, cards, cards, 0)
+
+	sum := 0
+	for _, v := range instances {
+		sum += v
+		//fmt.Printf("%v: %v\n", k, v)
+	}
+
+	return sum
 }
--- adventofcode/2023/day04/challenge_test.go
+++ adventofcode/2023/day04/challenge_test.go
@@ -11,5 +11,13 @@ func Test_sample1(t *testing.T) {
 }
 
 func TestMain_input1(t *testing.T) {
-	assert.Equal(t, part1(readInput("input.txt")), 53334, "they should be equal")
+	assert.Equal(t, part1(readInput("input.txt")), 23028, "they should be equal")
+}
+
+func Test_sample2(t *testing.T) {
+	assert.Equal(t, part2(readInput("sample.txt")), 30, "they should be equal")
+}
+
+func TestMain_input2(t *testing.T) {
+	assert.Equal(t, part2(readInput("input.txt")), 9236992, "they should be equal")
 }
