296a36eac80dfea35e45390bf8197d20dffe737a
Pedro Santos <donbonifacio@gmail.com>
2024-12-04 13:04:44 +0000

    Day04.2

--- adventofcode/2024/day04/challenge.go
+++ adventofcode/2024/day04/challenge.go
@@ -26,14 +26,17 @@ func part1(data *utils.ByteMatrix) int {
 		for x := 0; x < data.SizeX; x++ {
 			pos := utils.Position{X: x, Y: y}
 			for _, dir := range utils.VectorsAll {
-				sum += hasWord(data, "XMAS", dir, pos, "")
+				sum += hasWord(data, "XMAS", dir, pos)
 			}
 		}
 	}
 	return sum
 }
 
-func hasWord(data *utils.ByteMatrix, word string, dir utils.Vector, pos utils.Position, space string) int {
+func hasWord(data *utils.ByteMatrix, word string, dir utils.Vector, pos utils.Position) int {
+	if !data.Contains(pos) {
+		return 0
+	}
 	curr := data.Value(pos)
 	if len(word) == 1 && word[0] == curr {
 		return 1
@@ -41,14 +44,32 @@ func hasWord(data *utils.ByteMatrix, word string, dir utils.Vector, pos utils.Po
 	if word[0] != curr {
 		return 0
 	}
-	newWord := word[1:]
-	newPos := pos.Move(dir)
-	if data.Contains(newPos) {
-		return hasWord(data, newWord, dir, newPos, space+" ")
-	}
-	return 0
+	return hasWord(data, word[1:], dir, pos.Move(dir))
 }
 
-func part2(data utils.ByteMatrix) int {
-	return 0
+func part2(data *utils.ByteMatrix) int {
+	fmt.Println(data.ToString())
+	sum := 0
+	for y := 0; y < data.SizeY; y++ {
+		for x := 0; x < data.SizeX; x++ {
+			pos := utils.Position{X: x, Y: y}
+			curr := data.Value(pos)
+			if curr == 'A' {
+				topLeft := pos.Move(utils.Vector{X: -1, Y: -1})
+				bottomLeft := pos.Move(utils.Vector{X: -1, Y: 1})
+				diagonal1 := utils.Vector{X: 1, Y: 1}
+				diagonal2 := utils.Vector{X: 1, Y: -1}
+
+				hasDiagonal1 := hasWord(data, "MAS", diagonal1, topLeft) == 1 ||
+					hasWord(data, "SAM", diagonal1, topLeft) == 1
+				hasDiagonal2 := hasWord(data, "MAS", diagonal2, bottomLeft) == 1 ||
+					hasWord(data, "SAM", diagonal2, bottomLeft) == 1
+
+				if hasDiagonal1 && hasDiagonal2 {
+					sum += 1
+				}
+			}
+		}
+	}
+	return sum
 }
--- adventofcode/2024/day04/challenge_test.go
+++ adventofcode/2024/day04/challenge_test.go
@@ -11,5 +11,13 @@ func Test_sample1(t *testing.T) {
 }
 
 func TestMain_input1(t *testing.T) {
-	assert.Equal(t, part1(readInput("input.txt")), 53334, "they should be equal")
+	assert.Equal(t, part1(readInput("input.txt")), 2562, "they should be equal")
+}
+
+func Test_sample2(t *testing.T) {
+	assert.Equal(t, part2(readInput("sample.txt")), 9, "they should be equal")
+}
+
+func Test_input2(t *testing.T) {
+	assert.Equal(t, part2(readInput("input.txt")), 1902, "they should be equal")
 }
